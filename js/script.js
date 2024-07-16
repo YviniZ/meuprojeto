ed203139a9b8dd7c6394575ed6b330754a5cfd7e
YviniZ <131699650+YviniZ@users.noreply.github.com>
2024-07-16 16:03:24 -0300

    script.js

--- js/script.js
+++ js/script.js
@@ -113,11 +113,11 @@ function loadPage(page) {
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const toggleNav = document.getElementById("toggleNav");
   const mobileButtons = document.querySelector(".mobile-buttons");
 
-  toggleNav.addEventListener("click", function() {
+  toggleNav.addEventListener("click", function () {
     mobileButtons.classList.toggle("show-buttons");
     toggleNav.classList.toggle("show-nav");
   });
@@ -129,4 +129,4 @@ function scrollToSection(sectionId) {
     section.scrollIntoView({ behavior: 'smooth' });
   }
 }
-}
+
